91b52903c47578d433316823cced7467642e32b8
Yohan Ryoo <yohanryoo1@gmail.com>
2020-03-02 10:11:27 +1300

    need to define async function to activate page buttons

--- scripts/request.js
+++ scripts/request.js
@@ -9,6 +9,7 @@ const eventInfo = document.querySelector('.events-section');
 const pageWrapper = document.querySelector('.page-wrapper')
 const pageList = document.querySelector('.page-list')
 
+
 const imgPlaceholder = 'https://via.placeholder.com/140x100';
 const jsonImg = 'event.images.images[0]transforms.transforms[2].url';
 
@@ -16,6 +17,7 @@ const jsonImg = 'event.images.images[0]transforms.transforms[2].url';
 const getEvents = (genreID = 6, genreName = 'All gigs') => {
     const username = 'aucklandgigs';
     const password = '7fgrzf2gvz3j';
+    const field = 'fields=event:(name)';
     const url = `https://cors-anywhere.herokuapp.com/http://api.eventfinda.co.nz/v2/events.json?location=28&category=${genreID}&rows=10&offset=0`;
 
     const request = new XMLHttpRequest();
@@ -71,11 +73,47 @@ const pagination = (count) => {
 
     if (numberOfPages > 1) {
         for (let page = 1; page <= numberOfPages; page++) {
-            pageList.innerHTML += `<li><a href="#">${page}</a></li>`
+            pageList.innerHTML += `<li><a class="pg-link" href="#">${page}</a></li>`
         }
     }
 }
 
+getEvents()
+
+async function getData() {
+    const response = await getEvents()
+    console.log('Response received')
+}
+
+// promise or async function needed
+const pageButtons = document.getElementsByClassName('pg-link')
+console.log(pageButtons[0].innerText)
+
+
+
+// for (let i = 0; i < pageButtons.length; i++) {
+//     console.log('hi')
+// }
+
+//not working from here on
+// pageButtons.forEach(page => {
+//     page.addEventListener('click', e => {
+//         e.preventDefault();
+
+//         // refreshes page
+//         // eventInfo.innerHTML = '';
+//         // pageList.innerHTML = '';
+
+//         const pageNo = e.target.innerText;
+//         console.log(pageNo)
+//         // const genreID = ;
+//         // const genreName = ;
+
+//         // getEvents(genreID, genreName, )
+//     })
+// })
+
+// eventlisteners
 genreLink.forEach(genre => {
     genre.addEventListener('click', e => {
         e.preventDefault();
